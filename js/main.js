d557479f911d9e487fa69cfe99fb297ed8e76991
Andrew Gulik <andrewgulik@gmail.com>
2017-03-29 14:06:21 -0400

    fixed navigation, added full-screen overlay menu, added social links

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 
 // menu animation
 
-$(document).ready(function(){
-	$('#nav-icon3').click(function(){
-		$(this).toggleClass('open');
-	});
-});
+$('#toggle').click(function() {
+   $(this).toggleClass('active');
+   $('#overlay').toggleClass('open');
+  });
